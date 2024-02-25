7c110bb395481dd6b7a1ceda7c3c5a97a0ee7050
Rían Errity <rian@paradaux.io>
2024-02-25 03:23:19 +0000

    Update TDs

--- src/routers/HomeRouter.js
+++ src/routers/HomeRouter.js
@@ -2,21 +2,28 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/home/HomeScreen';
 import HomeBottomTab from '../components/navigation/HomeBottomTab';
 import ConstituencyScreen from '../screens/ConstituencyScreen';
+import TDScreen from '../screens/TDScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function HomeRouter() {
-  return (
-    <Tab.Navigator
-        initialRouteName='Home'
-        screenOptions={{
-        headerShown: false,
-        autoHideHomeIndicator: true,
-        }}
-        tabBar={HomeBottomTab}
-    >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Constituency" component={ConstituencyScreen} />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator
+            initialRouteName='Home'
+            screenOptions={{
+                headerShown: false,
+                autoHideHomeIndicator: true,
+            }}
+            tabBar={HomeBottomTab}
+        >
+            {/* <Tab.Screen name="TD" component={TDScreen} initialParams={{td: "Chris-Andrews.D.2007-06-14"}}/> */}
+            {/* <Tab.Screen name="Constituency" component={ConstituencyScreen} initialParams={{constituency: "Donegal"}}/> */}
+
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Constituency" component={ConstituencyScreen} />
+            <Tab.Screen name="TD" component={TDScreen} />
+
+
+        </Tab.Navigator>
+    );
 }
